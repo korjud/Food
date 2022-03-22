7548bbd360e18c4f442aa6a3ec317d8c11a522c5
Den <korsakds@gmail.com>
2022-03-22 22:20:22 +0300

    add fetch

--- js/script.js
+++ js/script.js
@@ -238,10 +238,7 @@ window.addEventListener('DOMContentLoaded', () => {
             `;
             form.insertAdjacentElement('afterend', statusMessage);
 
-            const request = new XMLHttpRequest();
-            request.open('POST', 'server.php');
 
-            request.setRequestHeader('Content-type', 'applicatiom/json');
             const formData = new FormData(form);
 
             const object = {};
@@ -250,18 +247,22 @@ window.addEventListener('DOMContentLoaded', () => {
                 object[key] = value;
             });
 
-            const json = JSON.stringify(object);
-            request.send(json);
-
-            request.addEventListener('load', () => {
-                if (request.status === 200) {
-                    console.log(request.response);
-                    showThanksModal(message.success);
-                    statusMessage.remove();
-                    form.reset();
-                } else {
-                    showThanksModal(message.failure);
-                }
+            
+            fetch('server.php', {
+                method: "POST",
+                headers: {
+                    'Content-type': 'applicatiom/json'
+                },
+                body: JSON.stringify(object)
+            }).then(data => data.text())
+            .then(data => {
+                console.log(data);
+                showThanksModal(message.success);
+                statusMessage.remove();
+            }).catch(() => {
+                showThanksModal(message.failure);
+            }).finally(() => {
+                form.reset();
             });
         });
     }
