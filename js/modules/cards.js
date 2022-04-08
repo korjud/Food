21791a3c901e7304610670a0a5e9107f5aa4e1cc
Den <korsakds@gmail.com>
2022-04-08 23:34:07 +0300

    add modules modification

--- js/modules/cards.js
+++ js/modules/cards.js
@@ -1,3 +1,5 @@
+import {getResource} from '../services/services';
+
 function cards() {
     class MenuCard {
         constructor(src, alt, title, descr, price, parentSelector, ...classes) {
@@ -47,15 +49,7 @@ function cards() {
             });
         });
 
-    async function getResource(url) {
-        let res = await fetch(url);
-        
-        if (!res.ok) {
-            throw new Error(`Could not fetch ${url}, status: ${res.status}`);
-        }
-        
-        return await res.json();
-    }
+    
 }
 
-module.exports = cards;
+export default cards;
